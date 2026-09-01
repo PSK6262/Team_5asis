58458c1359c6917c73ab277e12bda75b4d62f9ae
PSK6262 <qkrxotn60@gmail.com>
2026-09-01 15:26:45 +0900

    feature: 검색페이지의 UI 개선(css)

---
+++ src/main/webapp/resources/js/search.js
@@ -0,0 +1,9 @@
+function popularGamesOnclickEvent(gameName){
+	location.href = "/board/" + gameName;
+}
+function gameBoardPostClickEvent(gameAlias,pid){
+	location.href = "/board/" + gameAlias + "/" + pid;
+}
+function streamingCardClick(channelId){
+	location.href = "https://chzzk.naver.com/live/" + channelId;
+}
