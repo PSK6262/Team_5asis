49378eb184492dda07232ef6e8ce6c99e12dcf93
SudalSudal01 <sudalsudal1127@gmail.com>
2026-09-03 10:39:06 +0900

    feature : 보드 쪽 사이드바 프로필 연동 추가

--- src/main/java/com/app/controller/board/GameBoardController.java
+++ src/main/java/com/app/controller/board/GameBoardController.java
@@ -46,6 +46,21 @@ public class GameBoardController {
 		// 사이드바 전체 게임리스트
 		List<GameNameTransferForm> allGames = gameBoardService.findAllGames();
 		model.addAttribute("games", allGames);
+		// 로그인 사용자 닉네임, 프로필 이미지도 세션에서 꺼내서 전달
+				UserInfo loginUser = (UserInfo) session.getAttribute("LOGIN_USER");
+				if (loginUser != null) {
+					String nickname = loginUser.getNickname();
+					String profileImg = loginUser.getProfileImageUrl();
+					model.addAttribute("loginUser", loginUser);
+					model.addAttribute("nickname", nickname);
+					model.addAttribute("profileImg", profileImg);
+					System.out.println("닉네임: " + nickname);
+				} else {
+					// 로그인 안 된 상태 처리
+					model.addAttribute("nickname", "Guest");
+					model.addAttribute("profileImg",
+							"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhhyGGwgPL45lqvy3D15y74Heh7azl2cOLI7CPnHb6jw&s=10");
+				}
 
 		if (pSize == null || pSize <= 0) {
 			pSize = CommonCode.PAGING_SIZE_SMALL;
@@ -54,7 +69,7 @@ public class GameBoardController {
 			}
 		}
 		String gameName = gameBoardService.findGameNameByGameAlias(gameAlias);
-		UserInfo loginUser = (UserInfo)session.getAttribute("LOGIN_USER");
+		/* UserInfo loginUser = (UserInfo)session.getAttribute("LOGIN_USER"); */
 		System.out.println("alias : " + gameAlias + " page : " + page + " ctg : " + category + " pSize : " + pSize);
 		// 전체 List 불러오기
 		// List<Post> selectedPost =
